0a54b1458e9030663616977361d3180188c31ea7
unknown <shubhamakp@gmail.com>
2021-02-10 12:29:08 +0530

    lecture8

--- lecture8/index.js
+++ lecture8/index.js
@@ -2,3 +2,39 @@
 
 console.log("hello");
 console.log(2<3);
+
+var a = 12;
+
+if(a>10){
+    let z = 15;
+    var b = 20;
+    console.log("greater");
+}else{
+    console.log("smaller");
+}
+console.log(a);
+// console.log(z);
+console.log(b);
+
+const c = 2;
+console.log(c);
+// c=c+2;
+// console.log(c);
+
+
+// string concatenation
+
+var x = "hello";
+var y = " world";
+console.log(x+y);
+
+
+// Even odd code using if else 
+
+var value = 18;
+
+if(value % 2 == 0){
+    console.log(value + " is Even");
+}else{
+    console.log(value + " is Odd");
+}
